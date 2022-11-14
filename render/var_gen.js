f38aa9cf717d0f5240c4686e495d077c60bc1150
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2022-11-14 17:00:44 +0100

    feat(render): support string data

--- render/var_gen.js
+++ render/var_gen.js
@@ -1,10 +1,16 @@
 function var_gen(data){
+    if(typeof data === `string`){
+       data = JSON.parse(data);
+    };
+
     var result = ``
     for (var key in data) {
-        var var_content = `data.${key}`
-        result += `var ${key} = ${var_content};\n`;
+        var var_content = eval(`data.${key}`)
+        result += `var ${key} = "${var_content}";\n`;
     }
+    console.log(result);
     return result
 }
+
 
 export { var_gen }
