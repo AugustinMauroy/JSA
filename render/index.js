a565f6ce18f1a48121c3b316940a8efbbc874847
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2022-11-14 22:06:17 +0100

    feat(render): add path for template

--- render/index.js
+++ render/index.js
@@ -1,8 +1,21 @@
+import fs from "node:fs"
 import { var_gen } from "./var_gen.js"
 
 function render(insertJSA, data){
+    if(ispath.test(insertJSA) === true){
+        var insertJSA = fs.readFileSync(insertJSA, "utf-8", (err,data)=>{
+            if (err) {
+                throw err
+            } else {
+                return data
+            }
+        })
+    }
+
     let result = eval(`${var_gen(data)}\`${insertJSA}\``)
     return result;
 }
+
+const ispath = new RegExp ("^(.+)\/([^\/]+)$");
 
 export { render }
